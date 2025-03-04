f10d0378a6266b949d2890d8bfca1401e5a6f85b
Pablo Luberriaga <123048093+pablolube@users.noreply.github.com>
2025-03-04 11:27:03 -0300

    Add files via upload

---
+++ 0 - SQL - Interviews Questions/Amazon Interview Questions.sql
@@ -0,0 +1,231 @@
+-- Amazon Interview Practice 21/02/2024
+
+
+/* 1. You have two tables: Product and Supplier.
+- Product Table Columns: Product_id, Product_Name, Supplier_id, Price
+- Supplier Table Columns: Supplier_id, Supplier_Name, Country
+*/
+
+DROP DATABASE IF EXISTS Amazon_interview;
+CREATE DATABASE Amazon_interview;
+USE amazon_interview;
+
+-- creating supplier table 
+CREATE TABLE suppliers(supplier_id int PRIMARY KEY,
+					  supplier_name varchar(25),
+					  country VARCHAR(25));
+-- creating the product table 
+CREATE TABLE products(
+
+						product_id int PRIMARY KEY,
+						product_name VARCHAR(25),
+						supplier_id int,
+						price float,
+						FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id)
+						);
+
+INSERT INTO suppliers
+VALUES(501, 'Juan Perez', 'Peru'),
+		(502, 'John Lewosky', 'USA'),
+		(503, 'Pedro', 'Argentina'),
+		(504, 'Raul', 'USA'),
+		(505, 'Pamela Fernandez', 'México'),
+		(506, 'Maximiliano Gutierrez', 'México');
+
+
+INSERT INTO products
+VALUES	(201, 'iPhone 14', '501', 1299),
+		(202, 'iPhone 8', '502', 999),
+		(204, 'iPhone 13', '502', 1199),
+		(203, 'iPhone 11', '503', 1199),
+		(205, 'iPhone 12', '502', 1199),
+		(206, 'iPhone 14', '501', 1399),
+		(214, 'iPhone 15', '503', 1499),
+		(207, 'iPhone 15', '505', 1499),
+		(208, 'iPhone 15', '504', 1499),
+		(209, 'iPhone 12', '502', 1299),
+		(210, 'iPhone 13', '502', 1199),
+		(211, 'iPhone 11', '501', 1099),
+		(212, 'iPhone 14', '503', 1399),
+		(213, 'iPhone 8', '502', 1099),(222, 'Samsung Galaxy S21', '504', 1699),
+		(223, 'Samsung Galaxy S20', '505', 1899),
+		(224, 'Google Pixel 6', '501', 899),
+		(225, 'Google Pixel 5', '502', 799),
+		(226, 'OnePlus 9 Pro', '503', 1699),
+		(227, 'OnePlus 9', '502', 1999),
+		(228, 'Xiaomi Mi 11', '501', 899),
+		(229, 'Xiaomi Mi 10', '504', 699),
+		(230, 'Huawei P40 Pro', '505', 1099),
+		(231, 'Huawei P30', '502', 1299),
+		(232, 'Sony Xperia 1 III', '503', 1199),
+		(233, 'Sony Xperia 5 III', '501', 999),
+		(234, 'LG Velvet', '505', 1899),
+		(235, 'LG G8 ThinQ', '504', 799),
+		(236, 'Motorola Edge Plus', '502', 1099),
+		(237, 'Motorola One 5G', '501', 799),
+		(238, 'ASUS ROG Phone 5', '503', 1999),
+		(239, 'ASUS ZenFone 8', '504', 999),
+		(240, 'Nokia 8.3 5G', '502', 899),
+		(241, 'Nokia 7.2', '501', 699),
+		(242, 'BlackBerry Key2', '504', 1899),
+		(243, 'BlackBerry Motion', '502', 799),
+		(244, 'HTC U12 Plus', '501', 899),
+		(245, 'HTC Desire 20 Pro', '505', 699),
+		(246, 'Lenovo Legion Phone Duel', '503', 1499),
+		(247, 'Lenovo K12 Note', '504', 1499),
+		(248, 'ZTE Axon 30 Ultra', '501', 1299),
+		(249, 'ZTE Blade 20', '502', 1599),
+		(250, 'Oppo Find X3 Pro', '503', 1999);
+
+
+-- ----------------------------------------------
+-- My solution
+-- ----------------------------------------------
+
+-- Write an SQL query to find the name of the product with the highest price in each country.
+SELECT   
+    r.Country AS "Country",
+    r.Product_Name AS "Product Name",
+    r.max_price AS "Max Price"
+FROM (
+	SELECT 
+        s.country AS Country,
+        p.product_name AS Product_Name,
+        p.price AS max_price,
+        RANK() OVER (PARTITION BY s.country ORDER BY p.price DESC) AS ranking
+    FROM suppliers s
+    INNER JOIN products p 
+    ON s.supplier_id = p.supplier_id) AS r
+WHERE r.ranking = 1
+ORDER BY r.Country DESC, r.max_price DESC;
+
+
+-- Q.2 
+/* 
+You have two tables: Customer and Transaction.
+- Customer Table Columns: Customer_id, Customer_Name, Registration_Date
+- Transaction Table Columns: Transaction_id, Customer_id, Transaction_Date, Amount
+
+*/
+
+DROP DATABASE IF EXISTS Amazon_interview;
+CREATE DATABASE Amazon_interview;
+USE amazon_interview;
+-- Create Customer table
+CREATE TABLE Customers (
+    Customer_id INT PRIMARY KEY,
+    Customer_Name VARCHAR(100),
+    Registration_Date DATE
+);
+
+-- Create Transaction table
+CREATE TABLE Transaction (
+    Transaction_id INT PRIMARY KEY,
+    Customer_id INT,
+    Transaction_Date DATE,
+    Amount DECIMAL(10, 2),
+    FOREIGN KEY (Customer_id) REFERENCES Customers(Customer_id)
+);
+
+-- Insert records into Customer table
+INSERT INTO Customers (Customer_id, Customer_Name, Registration_Date)
+VALUES
+    (1, 'John Doe', '2023-01-15'),
+    (2, 'Jane Smith', '2023-02-20'),
+    (3, 'Michael Johnson', '2023-03-10');
+
+-- Insert records into Transaction table
+INSERT INTO Transaction (Transaction_id, Customer_id, Transaction_Date, Amount)
+VALUES
+    (201, 1, '2024-01-20', 50.00),
+    (202, 1, '2024-02-05', 75.50),
+    (203, 2, '2023-02-22', 100.00),
+    (204, 3, '2022-03-15', 200.00),
+    (205, 2, '2024-03-20', 120.75),
+	(301, 1, '2024-01-20', 50.00),
+    (302, 1, '2024-02-05', 75.50),
+    (403, 2, '2023-02-22', 100.00),
+    (304, 3, '2022-03-15', 200.00),
+    (505, 2, '2024-03-20', 120.75);
+
+SELECT * FROM customers;
+SELECT * FROM transaction;
+
+-- Write an SQL query to calculate the total transaction amount for each customer for the current year. 
+--  The output should contain Customer_Name and the total amount.
+
+-- ----------------------------------------------
+-- My solution
+-- ----------------------------------------------
+
+Set @current_year=2024;
+
+SELECT c.customer_name,sum(t.Amount) as 'Total Amount' 
+FROM customers c
+inner JOIN transaction as  t
+ON c.customer_id=t.customer_id
+WHERE YEAR(t.transaction_Date)=@current_year
+group by c.customer_id;
+
+
+
+-- SCHEMA
+
+
+-- Create pages table
+CREATE TABLE pages (
+    page_id INTEGER PRIMARY KEY,
+    page_name VARCHAR(255)
+);
+
+-- Insert data into pages table
+INSERT INTO pages (page_id, page_name) VALUES
+(20001, 'SQL Solutions'),
+(20045, 'Brain Exercises'),
+(20701, 'Tips for Data Analysts');
+
+-- Create page_likes table
+CREATE TABLE page_likes (
+    user_id INTEGER,
+    page_id INTEGER,
+    liked_date DATETIME,
+    FOREIGN KEY (page_id) REFERENCES pages(page_id)
+);
+
+-- Insert data into page_likes table
+INSERT INTO page_likes (user_id, page_id, liked_date) VALUES
+(111, 20001, '2022-04-08 00:00:00'),
+(121, 20045, '2022-03-12 00:00:00'),
+(156, 20001, '2022-07-25 00:00:00');
+
+
+
+/*
+Question 1:
+Write a SQL query to retrieve the IDs of the Facebook pages that have zero likes. 
+The output should be sorted in ascending order based on the page IDs.
+*/
+
+-- Question 1 link ::  https://datalemur.com/questions/sql-page-with-no-likes
+
+-- ------------------------------------------------------------------------------------------------------------------------------------
+-- My Solution
+-- ------------------------------------------------------------------------------------------------------------------------------------
+
+
+SELECT p.page_id
+FROM pages p
+LEFT JOIN page_likes pl ON p.page_id = pl.page_id
+GROUP BY p.page_id
+HAVING COUNT(pl.page_id) = 0
+ORDER BY p.page_id ASC;
+
+
+
+
+
+
+
+
+
+
